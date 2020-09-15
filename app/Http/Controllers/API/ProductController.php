8ec09b9a21f53cbed7cde1d504b850e2b35af55c
devcarloshenrique <devcarloshenrique@gmail.com>
2020-09-15 08:43:44 -0300

    refactor: listing all products

--- app/Http/Controllers/API/ProductController.php
+++ app/Http/Controllers/API/ProductController.php
@@ -4,9 +4,17 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class ProductController extends Controller
 {
+
+    private $product;
+
+    public function __construct(Product $product)
+    {
+        $this->product = $product;
+    }
     /**
      * Display a listing of the resource.
      *
@@ -14,7 +22,8 @@ class ProductController extends Controller
      */
     public function index()
     {
-        return response()->json(['sucess' => 'Index']);
+        $products = $this->product->all();
+        return response()->json(['data' => $products]);
     }
 
     /**
