c222c31cef1e06f3984ce1d14d9ae53f23f28a42
devcarloshenrique <devcarloshenrique@gmail.com>
2020-09-18 09:55:16 -0300

    refactor: cors configured for web requests

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -42,6 +42,9 @@ class Kernel extends HttpKernel
         'api' => [
             'throttle:60,1',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+
+            // Middleware CORS, somente na api
+            \App\Http\Middleware\Cors::class,
         ],
     ];
 
